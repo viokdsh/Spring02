782bcfde44e5d85efeac823a445f83a40f6a4d71
viokdsh <oraragi@skuniv.ac.kr>
2025-08-06 17:35:31 +0900

    1735

--- src/main/java/pack/Spring02Application.java
+++ src/main/java/pack/Spring02Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Spring02Application {
 	@GetMapping("/")
 	public String tiger() {
-		return "Spring02:0911";
+		return "Spring02:1735";
 	}
 	
 	public static void main(String[] args) {
